80c3eb9a82c9607f2759ea6f66b938c4829520b6
Andrew1481432 <andrew1481432@mail.ru>
2024-05-28 18:32:46 +0300

    add(olo): error logging

--- olo_service/internal/service/olo_service.go
+++ olo_service/internal/service/olo_service.go
@@ -34,8 +34,14 @@ func NewOloService(log *slog.Logger, repo *repository.Repository) *OloService {
 // GetWidgets retrieves widgets associated with a specific user from the repository.
 func (s *OloService) GetWidgets(userId int64) ([]entity.Widget, error) {
 	const op = "olo.GetWidgets"
+
+	log := s.log.With(
+		slog.String("op", op),
+		slog.Int64("userId", userId))
+
 	widgets, err := s.repo.GetWidgets(userId)
 	if err != nil {
+		log.Error("failed get widgets", sl.Err(err))
 		return nil, sl.Wrap(op, fmt.Errorf("can't get widgets"))
 	}
 	return widgets, nil
@@ -44,8 +50,15 @@ func (s *OloService) GetWidgets(userId int64) ([]entity.Widget, error) {
 // UpdateWidget adds a widget for a specific user.
 func (s *OloService) UpdateWidget(data string, widgetId, userId int64) error {
 	const op = "olo.UpdateWidget"
+
+	log := s.log.With(
+		slog.String("op", op),
+		slog.Int64("userId", userId),
+		slog.Int64("widgetId", widgetId))
+
 	err := s.repo.UpdateWidget(data, widgetId, userId)
 	if err != nil {
+		log.Error("failed update widget", sl.Err(err))
 		return sl.Wrap(op, fmt.Errorf("can't update widget"))
 	}
 	return nil
@@ -54,8 +67,14 @@ func (s *OloService) UpdateWidget(data string, widgetId, userId int64) error {
 // AddWidget adds a widget for a specific user.
 func (s *OloService) AddWidget(data string, userId int64) (int64, error) {
 	const op = "olo.AddWidget"
+
+	log := s.log.With(
+		slog.String("op", op),
+		slog.Int64("userId", userId))
+
 	widgetId, err := s.repo.AddWidget(data, userId)
 	if err != nil {
+		log.Error("failed add widget", sl.Err(err))
 		return 0, sl.Wrap(op, fmt.Errorf("can't add widget"))
 	}
 	return widgetId, nil
@@ -64,8 +83,14 @@ func (s *OloService) AddWidget(data string, userId int64) (int64, error) {
 // DeleteWidgetForUser delete a widget for a specific user.
 func (s *OloService) DeleteWidgetForUser(widgetId, userId int64) error {
 	const op = "olo.DeleteWidgetForUser"
+
+	log := s.log.With(
+		slog.String("op", op),
+		slog.Int64("userId", userId))
+
 	err := s.repo.DeleteWidget(widgetId, userId)
 	if err != nil {
+		log.Error("failed delete widget", sl.Err(err))
 		return sl.Wrap(op, fmt.Errorf("can't delete widget for user"))
 	}
 	return nil
@@ -74,8 +99,13 @@ func (s *OloService) DeleteWidgetForUser(widgetId, userId int64) error {
 // GetAllArticles retrieves all articles from the repository.
 func (s *OloService) GetAllArticles() ([]entity.Article, error) {
 	const op = "olo.GetAllArticles"
+
+	log := s.log.With(
+		slog.String("op", op))
+
 	articles, err := s.repo.GetAllArticles()
 	if err != nil {
+		log.Error("failed get articles", sl.Err(err))
 		return nil, sl.Wrap(op, fmt.Errorf("can't get all articles"))
 	}
 	return articles, nil
@@ -84,8 +114,14 @@ func (s *OloService) GetAllArticles() ([]entity.Article, error) {
 // GetUsersArticles retrieves articles associated with a specific user from the repository.
 func (s *OloService) GetUsersArticles(userId int64) ([]entity.Article, error) {
 	const op = "olo.GetUsersArticles"
+
+	log := s.log.With(
+		slog.String("op", op),
+		slog.Int64("userId", userId))
+
 	articles, err := s.repo.GetUsersArticles(userId)
 	if err != nil {
+		log.Error("failed get articles of user", sl.Err(err))
 		return nil, sl.Wrap(op, fmt.Errorf("can't get articles of user"))
 	}
 	return articles, nil
@@ -94,8 +130,15 @@ func (s *OloService) GetUsersArticles(userId int64) ([]entity.Article, error) {
 // AddArticleForUser adds an article for a specific user.
 func (s *OloService) AddArticleForUser(articleId, userId int64) error {
 	const op = "olo.AddArticleForUser"
+
+	log := s.log.With(
+		slog.String("op", op),
+		slog.Int64("articleId", articleId),
+		slog.Int64("userId", userId))
+
 	err := s.repo.AddArticleForUser(articleId, userId)
 	if err != nil {
+		log.Error("failed add article of user", sl.Err(err))
 		return sl.Wrap(op, fmt.Errorf("can't add article for user"))
 	}
 	return nil
@@ -103,8 +146,15 @@ func (s *OloService) AddArticleForUser(articleId, userId int64) error {
 
 func (s *OloService) DeleteArticleForUser(articleId int64, userId int64) error {
 	const op = "olo.DeleteArticleForUser"
+
+	log := s.log.With(
+		slog.String("op", op),
+		slog.Int64("articleId", articleId),
+		slog.Int64("userId", userId))
+
 	err := s.repo.DeleteArticleForUser(articleId, userId)
 	if err != nil {
+		log.Error("failed delete article", sl.Err(err))
 		return sl.Wrap(op, fmt.Errorf("can't delete article"))
 	}
 	return nil
